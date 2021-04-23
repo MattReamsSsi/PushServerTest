75398930b179116ed4d340dbf32708f1d87f9a59
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-04-23 18:17:42 -0400

    can send matt-topic

--- PushServerTest/Controllers/WeatherForecastController.cs
+++ PushServerTest/Controllers/WeatherForecastController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -34,6 +37,39 @@ namespace PushServerTest.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("DoSomething")]
+        public async Task<string> DoSomething()
+        {
+            NotificationSender.Init();
+            var message = new Message()
+            {
+                Notification = new Notification()
+                {
+                    Title = "Matt Message Try",
+                    Body = "the body of the message.",
+                },
+                Topic = "matt-topic",
+            };
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            return "Successfully sent message: " + response;
+        }
+    }
+
+    static class NotificationSender
+    {
+        private static bool _initted;
+        public static void Init()
+        {
+            if (!_initted)
+            {
+                FirebaseApp.Create(new AppOptions
+                {
+                    Credential = GoogleCredential.FromFile("pushtesting-d1828-firebase-adminsdk-qpj35-c8ed98a3fe.json")
+                });
+                _initted = true;
+            }
         }
     }
 }
