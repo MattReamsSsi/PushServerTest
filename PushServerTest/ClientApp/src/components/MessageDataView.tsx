4fe0efc9c0c7c3adee3315f9ed28df5ac5325fd0
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-04-27 13:45:35 -0400

    has another form for message data

---
+++ PushServerTest/ClientApp/src/components/MessageDataView.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const MessageDataView = () => {
+    return (
+        <div>
+          <h1>Hello from message view</h1>
+        </div>
+      );
+}
+
+export default MessageDataView;
