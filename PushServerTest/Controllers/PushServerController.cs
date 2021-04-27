b485f6656c773cff55f83f448310b9548ddc807d
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-04-27 08:14:32 -0400

    can send message from new API

--- PushServerTest/Controllers/PushServerController.cs
+++ PushServerTest/Controllers/PushServerController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
 using Microsoft.AspNetCore.Mvc;
 using PushServerTest.Persistence;
 
@@ -30,16 +33,49 @@ namespace PushServerTest.Controllers
         }
 
         [HttpPost("SendPushMessage")]
-        public IActionResult SendPushMessage(PushMessage pushMessage)
+        public async Task<string> SendPushMessage(PushMessage pushMessage)
         {
+            var ret = await PushMessageSender.SendPushMessage(pushMessage);
             PushServerDatabase.UpdateMessageCount(pushMessage);
-            return new OkResult();
+            return ret;
         }
 
         [HttpGet("GetUserDatas")]
         public List<UserData> GetUserDatas()
         {
             return PushServerDatabase.GetUserDatas();
+        }
+    }
+
+    static class PushMessageSender
+    {
+        public static async Task<string> SendPushMessage(PushMessage pushMessage)
+        {
+            Init();
+            var message = new Message
+            {
+                Notification = new Notification
+                {
+                    Title = pushMessage.Title,
+                    Body = pushMessage.MessageBody
+                },
+                Topic = $"ssi-topic-{pushMessage.ApiClientId}-{pushMessage.UserId}"
+            };
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            return "Successfully sent message: " + response;
+        }
+
+        private static bool _initted;
+        public static void Init()
+        {
+            if (!_initted)
+            {
+                FirebaseApp.Create(new AppOptions
+                {
+                    Credential = GoogleCredential.FromFile("pushtesting-d1828-firebase-adminsdk-qpj35-c8ed98a3fe.json")
+                });
+                _initted = true;
+            }
         }
     }
 }
