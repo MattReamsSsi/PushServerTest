6792b0612c840401739f3d7ad977be539edaa104
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-04-30 16:28:16 -0400

    trying a chakra table for the data.

--- PushServerTest/ClientApp/src/components/MessageDataView.tsx
+++ PushServerTest/ClientApp/src/components/MessageDataView.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+ } from "@chakra-ui/react";
 
 import {
     fetchAll,
@@ -24,6 +34,42 @@ const MessageDataView = () => {
           <h2>{status}</h2>
           <h3>{JSON.stringify(apiClientDatas)}</h3>
           <h4>{JSON.stringify(userDatas)}</h4>
+
+          <Table variant="simple">
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>inches</Td>
+                <Td>millimetres (mm)</Td>
+                <Td isNumeric>25.4</Td>
+              </Tr>
+              <Tr>
+                <Td>feet</Td>
+                <Td>centimetres (cm)</Td>
+                <Td isNumeric>30.48</Td>
+              </Tr>
+              <Tr>
+                <Td>yards</Td>
+                <Td>metres (m)</Td>
+                <Td isNumeric>0.91444</Td>
+              </Tr>
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+
         </div>
       );
 }
