dac1840cffa19a28f5e0019a59d4a805eb4a680a
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-05-25 10:10:28 -0400

    removes some unneeded files, and makes App a functional component.

--- PushServerTest/ClientApp/src/App.js
+++ PushServerTest/ClientApp/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { FetchData } from './components/FetchData';
 import MessageDataView from './components/MessageDataView';
 import ApiClientsView from './components/ApiClientsView';
 import UserDataView from './components/UserDataView';
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={ApiClientsView} />
-        <Route path='/user-data' component={UserDataView} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/message-data' component={MessageDataView} />
-        <Route path='/api-clients' component={ApiClientsView} />
-      </Layout>
-    );
-  }
+const App = () =>  {
+  return (
+    <Layout>
+      <Route exact path='/' component={ApiClientsView} />
+      <Route path='/user-data' component={UserDataView} />
+      <Route path='/message-data' component={MessageDataView} />
+      <Route path='/api-clients' component={ApiClientsView} />
+    </Layout>
+  );
 }
+
+export default App;
