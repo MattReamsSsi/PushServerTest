38c65468d0b1da68e5d6a8920310997d95abf1bf
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-05-25 14:02:19 -0400

    working on adding api client dialog

--- PushServerTest/ClientApp/src/components/ApiClientsView.tsx
+++ PushServerTest/ClientApp/src/components/ApiClientsView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
     Table,
     Thead,
@@ -10,7 +10,18 @@ import {
     TableCaption,
     Checkbox,
     Button,
-    VStack
+    VStack,
+    Input,
+    Text,
+    //Modal
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure
 } from "@chakra-ui/react";
 import { ApiClientData } from '../DataStructures';
 import {
@@ -25,11 +36,15 @@ const ApiClientsView = () => {
     const apiClientDatasToShow = includeDeleted ? apiClientDatas : apiClientDatas.filter(v => !v.isDeleted);
     const history = useHistory();
 
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <div>
 
+            <AddApiClientModal isOpen={isOpen} onClose={onClose} />
+
             <VStack spacing="24px" align='flex-start'>
-                <Button colorScheme="blue">Add API-Client</Button>
+                <Button colorScheme="blue" onClick={onOpen}>Add API-Client</Button>
                 <Checkbox
                     isChecked={includeDeleted}
                     onChange={() => setIncludeDeleted(!includeDeleted)}>
@@ -68,6 +83,67 @@ const ApiClientsView = () => {
             </Table>
         </div>
     );
+}
+
+const guidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+const AddApiClientModal = ({ isOpen, onClose }: any) => {
+
+    const dispatch = useDispatch();
+    const [userGuid, setUserGuid] = useState("");
+    const [apiGuid, setApiGuid] = useState("");
+    const [userGuidIsValid, setUserGuidIsValid] = useState(false);
+    const [apiGuidIsValid, setApiGuidIsValid] = useState(false);
+    const [userDescription, setUserDescription] = useState("");
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Add User</ModalHeader>
+                <ModalCloseButton />
+
+                <ModalBody>
+                    <div>
+                        <h2>Add User:</h2>
+                        <Text>User Description: {userDescription}</Text>
+                        <Input placeholder="user description" value={userDescription} onChange={v => {
+                            const val = (v.target as any).value;
+                            setUserDescription(val);
+                        }} />
+                        <Text>UserGuid: {userGuid}, Is Valid: {userGuidIsValid.toString()}</Text>
+                        <Input placeholder="userGuid" value={userGuid} onChange={v => {
+                            const val = (v.target as any).value;
+                            setUserGuid(val);
+                            setUserGuidIsValid(guidPattern.test(val));
+                        }} />
+                        <Text>ApiGuid: {apiGuid}, Is Valid: {apiGuidIsValid.toString()}</Text>
+                        <Input placeholder="apiGuid" value={apiGuid} onChange={v => {
+                            const val = (v.target as any).value;
+                            setApiGuid(val);
+                            setApiGuidIsValid(guidPattern.test(val));
+                        }} />
+
+                    </div>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={onClose}>
+                        Close
+              </Button>
+                    <Button
+                        colorScheme="blue"
+                        isDisabled={!(userGuidIsValid && apiGuidIsValid)}
+                        onClick={() => {
+                            //dispatch((adduserData as any)({ id: userGuid, apiClientId: apiGuid, messagesCount: 0, description: userDescription }));
+                            onClose();
+                        }}>
+                        Commit User Data
+                </Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
 }
 
 export default ApiClientsView;
